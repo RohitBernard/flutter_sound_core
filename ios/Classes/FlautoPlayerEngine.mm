94d0acb6e67bbef7c6e98fe112dd53ecbb0c6a62
Pavel <pavel@invertase.io>
2025-01-01 20:55:30 -0800

    feat: bring back voice processing to test smth

--- ios/Classes/FlautoPlayerEngine.mm
+++ ios/Classes/FlautoPlayerEngine.mm
@@ -178,6 +178,21 @@
                 engine = [[AVAudioEngine alloc] init];
                 outputNode = [engine outputNode];
                 NSLog(@"[AudioEngine] Audio engine created: %.3fms", (CACurrentMediaTime() - startTime) * 1000);
+           
+                if (@available(iOS 13.0, *)) {
+                    if ([flutterSoundPlayer isVoiceProcessingEnabled]) {
+                        NSLog(@"[AudioEngine] Enabling voice processing");
+                        NSError* err;
+                        if (![outputNode setVoiceProcessingEnabled:YES error:&err]) {
+                           [flutterSoundPlayer logDebug:[NSString stringWithFormat:@"error enabling voiceProcessing => %@", err]];
+                        } else {
+                            [flutterSoundPlayer logDebug: @"VoiceProcessing enabled"];
+                        }
+                        NSLog(@"[AudioEngine] Voice processing setup: %.3fms", (CACurrentMediaTime() - startTime) * 1000);
+                    }
+                } else {
+                   [flutterSoundPlayer logDebug: @"WARNING! VoiceProcessing is only available on iOS13+"];
+                }
                
                 NSLog(@"[AudioEngine] Getting output format");
                 outputFormat = [outputNode inputFormatForBus: 0];
